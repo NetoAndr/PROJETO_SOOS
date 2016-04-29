ca570d76cd6591b8b9c9c374820a594b5ad41ae9
Manoel Andrade <netoandr@hotmail.com>
2016-04-29 20:43:47 -0300

    testes caso 1 add

--- src/projeto/Facade.java
+++ src/projeto/Facade.java
@@ -1,11 +1,20 @@
 package projeto;
 
+import easyaccept.EasyAccept;
+
 public class Facade {
 	
 	HospitalController controller;
 	
 	Facade(){
 		controller = new HospitalController();
+	}
+	
+	public static void main(String[] args) {
+	    args = new String[] {
+	    		"projeto.Facade", 
+	    		"recursos/scripts de testes/usecase_1.txt"}; //separe cada script de teste por virgula.
+	    EasyAccept.main(args);
 	}
 	
 	public boolean liberaSistema(String chave){
